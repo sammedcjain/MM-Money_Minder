8a4b9ae84527c52f66a566f6ea3e636b6a920c3c
sammedcjain <2020is_sammedcjain_b@nie.ac.in>
2024-03-06 20:37:39 +0530

    added payment method

--- server/models/expense.js
+++ server/models/expense.js
@@ -1,28 +1,33 @@
-const mongoose = require('mongoose')
-const User = require('./user.js')
+const mongoose = require("mongoose");
+const User = require("./user.js");
 const expenseScehma = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  expense: [
+    {
+      Amount: {
+        type: Number,
+        required: true,
+      },
+      category: {
+        type: String,
+        default: "NA",
+        required: true,
+      },
+      description: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+      payment_method: {
+        type: String,
+      },
     },
-    expense: [{
-        Amount: {
-            type: Number,
-            required: true
-        },
-        category: {
-            type: String,
-            default: "NA",
-            required: true
-        },
-        description: {
-            type: String
-        },
-        date: {
-            type: Date,
-            default: Date.now
-        }
-    }],
-})
-const Expense = new mongoose.model('Expense', expenseScehma)
-module.exports = Expense
+  ],
+});
+const Expense = new mongoose.model("Expense", expenseScehma);
+module.exports = Expense;
